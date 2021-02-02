709e8662a71fc7f7ca80848c1098c9adfa87cc42
retropfilmz <61301467+retropfilmz@users.noreply.github.com>
2021-02-02 11:26:58 -0800

    Tied pages to index and tidied up file structure.
    Added links between pages to allow for test of the PHP files. Moved avatars and CSS files to dedicated folders to clean up the file structure.

---
+++ sadserver/php/login.php
@@ -0,0 +1,29 @@
+<html>
+
+<head>
+  <title>Login</title>
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="/css/style.css">
+
+<body>
+
+  <div class="logincontainer">
+    <img src="/resources/avatar.png" class="avatar">
+    <h1>Login Here </h1>
+    <form>
+      <p>Username</p>
+      <input type="text" name="" placeholder="Enter Username">
+      <p>Password</p>
+      <input type="text" name="" placeholder="Enter Password">
+      <input type="submit" name="" value="Login">
+      <a href="signup.php">Don't have an account?</a>
+	  <br>
+	  <a href="accountinfo.php">Placeholder to skip login mechanism</a>
+    </form>
+  </div>
+
+</body>
+</head>
+
+</html>

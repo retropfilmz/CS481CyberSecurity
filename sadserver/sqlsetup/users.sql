3d85bc968f24c309cc00642cbb0b4f9cdef24d8a
retropfilmz <61301467+retropfilmz@users.noreply.github.com>
2021-03-09 21:34:13 -0800

    Chanced ID to UserID
    Allowing for easier understanding of the relational database.

--- sadserver/sqlsetup/users.sql
+++ sadserver/sqlsetup/users.sql
@@ -1,7 +1,6 @@
 CREATE TABLE `users` (
-  `Userid` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `UserID` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `username` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
-  `password` varchar(100) NOT NULL,
-  `address` varchar(100) NOT NULL
+  `password` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
